753124b804d3282ff3f6bb4cba489c95a813f161
jonpedigo <pedigojon@gmail.com>
2023-02-08 17:27:29 -0800

    cleaned up the ids a little bit

--- client/src/constants/interfaceIds/cameraInterfaceIds.js
+++ client/src/constants/interfaceIds/cameraInterfaceIds.js
@@ -1,3 +1,3 @@
 export const CAMERA_ZOOM_IID = 'camera/zoom'
-export const CAMERA_LERP_X_IID = 'camera/lerp/horizontal'
-export const CAMERA_LERP_Y_IID = 'camera/lerp/vertical'
+export const CAMERA_LERP_X_IID = 'camera/lerpX'
+export const CAMERA_LERP_Y_IID = 'camera/lerpY'
